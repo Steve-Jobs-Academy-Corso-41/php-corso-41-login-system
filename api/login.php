a9a171732351f7067e5348b9b2844a8f05447e85
Flame0510 <micheletornello5@gmail.com>
2026-03-04 12:51:12 +0100

    feat: add sessions

--- api/login.php
+++ api/login.php
@@ -1,4 +1,6 @@
 <?php
+session_start(); // Avvia la sessione
+
 // Esempio di login semplice con PHP
 // Verifico se il form è stato inviato con il metodo POST tramite la superglobale $_SERVER
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -8,23 +10,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Validazione dell'email
     if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email format.";
+        $_SESSION['login_error'] = "Invalid email format.";
+        header('Location: ../login.php'); // Reindirizza alla pagina di login
         exit;
     }
 
     // Validazione del campo password
     if (empty($password)) {
-        echo "Password field is required.";
+        $_SESSION['login_error'] = "Password field is required.";
+        header('Location: ../login.php'); // Reindirizza alla pagina di login
         exit;
     }
 
     // Validazione della password
     if (strlen($password) < 8) {
-        echo "Password must be at least 8 characters long.";
+        $_SESSION['login_error'] = "Password must be at least 8 characters long.";
+        header('Location: ../login.php'); // Reindirizza alla pagina di login
         exit;
     }
 
     // Dati di esempio per l'autenticazione con costanti definiti
+    define('USER_NAME', "mimmoquaqquaraqqua");
     define('USER_EMAIL', 'user@example.com');
     define('USER_PASSWORD', '$2y$10$wlgEg8NQwxC2LqknbiFaFuFA8VI3j0cAP85xIorJPQkr/qF8NhlMa'); // Hash della password per sicurezza
 
@@ -32,11 +38,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Controllo se le credenziali sono corrette
     if ($email === USER_EMAIL && password_verify($password, USER_PASSWORD)) {
         // Autenticazione riuscita
+
+        session_start(); // Avvia la sessione
+        $_SESSION['user_name'] = USER_NAME; // Memorizza il nome dell'utente
+
         header('Location: ../dashboard.php'); // Reindirizza a una pagina protetta
         exit;
     } else {
         // Autenticazione fallita
-        echo "Invalid email or password.";
+        $_SESSION['login_error'] = "Invalid email or password.";
+        header('Location: ../login.php'); // Reindirizza alla pagina di login
         exit;
     }
 }
